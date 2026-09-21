121fa7fc8950e38e14d9c59a766827e831548f21
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2026-09-21 19:34:07 -0400

    First commit. [skip ci]

---
+++ pgo-build.ps1
@@ -0,0 +1,52 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2026 Nihilai Collective Corp
+# https://github.com/nihilai-collective/stringint-benchmarks
+# pgo-build.ps1
+
+param(
+    [string]$WorkDir = $PSScriptRoot,
+    [ValidateSet("ON", "OFF")]
+    [string]$SibCiRun = $(if ($env:CI) { "ON" } else { "OFF" })
+)
+
+Write-Host "SIB_CI_RUN: $SibCiRun"
+
+Set-Location $WorkDir
+
+$PgoDataDir = "$WorkDir\pgo-data"
+$InstallPath = "$WorkDir\Install"
+
+if (-not (Test-Path $PgoDataDir)) {
+    New-Item -ItemType Directory -Path $PgoDataDir | Out-Null
+}
+
+$vsWhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+$vsPath = & $vsWhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+$vcvars = "$vsPath\VC\Auxiliary\Build\vcvars64.bat"
+
+cmd /c "`"$vcvars`" && cmake -S . -B ./Build-Gen -DCMAKE_BUILD_TYPE=Release -DPGO_MODE=GENERATE -DPGO_DATA_DIR=`"$PgoDataDir`" -DSIB_CI_RUN=$SibCiRun"
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+cmd /c "`"$vcvars`" && cmake --build ./Build-Gen --config=Release -v"
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+$trainingBin = Get-ChildItem -Path "./Build-Gen" -Recurse -Filter "StringInt-Benchmarks.exe" | Select-Object -First 1
+if ($trainingBin) {
+    cmd /c "`"$vcvars`" && `"$($trainingBin.FullName)`""
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+} else {
+    Write-Error "Training binary not found"
+    exit 1
+}
+
+cmd /c "`"$vcvars`" && cmake -S . -B ./Build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$InstallPath`" -DPGO_MODE=USE -DPGO_DATA_DIR=`"$PgoDataDir`" -DSIB_CI_RUN=$SibCiRun"
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+cmd /c "`"$vcvars`" && cmake --build ./Build --config=Release -v"
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+Push-Location ./Build
+cmd /c "`"$vcvars`" && cmake --install . --config=Release"
+Pop-Location
+
+$finalBin = "$InstallPath\bin\StringInt-Benchmarks.exe"
